ca68f1e17a4dba11d683909ba584b717993b25dd
corclark <corclark@gmail.com>
2018-02-21 21:35:19 -0700

    Minor changes for example

--- Assets/Scripts/BetterJump.cs
+++ Assets/Scripts/BetterJump.cs
@@ -11,22 +11,19 @@ public class BetterJump : MonoBehaviour {
 
 	void Awake () {
         rb = GetComponent<Rigidbody2D>();
-		
 	}
 
 
-    void FixedUpdate()
-    {
-        if (rb.velocity.y < 0){
+    void FixedUpdate() {
+        if (rb.velocity.y < 0) {
             rb.gravityScale = fallMultiplier;
         }
-        else if (rb.velocity.y > 0 && !Input.GetButton("Jump")){
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {
             rb.gravityScale = lowJumpMultiplier;
 		}
 
 		else {
             rb.gravityScale = 1f;
         }
-
     }
 }
