c199621fd7ddd37a9b08a9481e0bed708d08b010
Nimanski <36685865+Nimanski@users.noreply.github.com>
2018-02-23 17:32:41 -0600

    no change

--- Assets/Scripts/Grab_Script.cs
+++ Assets/Scripts/Grab_Script.cs
@@ -39,7 +39,7 @@ public class Grab_Script : MonoBehaviour {
     private void GrabObject()
     {
         Physics2D.queriesStartInColliders = false;
-        hit = Physics2D.Raycast(rayPoint.position, Vector2.right * transform.localScale.x, distance);
+		hit = Physics2D.Raycast(rayPoint.position, Vector2.right * transform.localScale.x, distance);
 
         if (hit.collider != null && hit.collider.tag == "grabbable")
         {
