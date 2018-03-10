bc4fe36186c86a6504da643e67000cc4bf518600
Nimanski <36685865+Nimanski@users.noreply.github.com>
2018-03-09 19:31:25 -0600

    Ball Trap
    Added Ball Trap and made test level

---
+++ Assets/Scripts/Ball_Trap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Trap : MonoBehaviour {
+
+    private Ball_Health ball;
+
+    void Start()
+    {
+
+        ball = GameObject.FindGameObjectWithTag("grabbable").GetComponent<Ball_Health>();
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.transform.CompareTag("grabbable"))
+        {
+
+            ball.BDamage(1);
+
+        }
+
+    }
+}
