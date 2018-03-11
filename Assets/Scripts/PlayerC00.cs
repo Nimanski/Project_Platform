0aca7afb3d9586f8e1380e9705793a81012efefb
Nimanski <36685865+Nimanski@users.noreply.github.com>
2018-03-11 14:53:58 -0500

    PlayerController

---
+++ Assets/Scripts/PlayerC00.cs
@@ -0,0 +1,48 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//[RequireComponent (typeof (Controller2D))]
+//public class PlayerC : MonoBehaviour {
+
+//    public float jumpHeight = 4;
+//    public float timeToJumpApex = .4f;
+//    float moveSpeed = 6;
+//    public float accelerationTimeAirborne = .2f;
+//    public float accelerationTimeGrounded = .1f;
+//    float velocityXSmoothing;
+
+//    public float gravity;
+//    public float jumpVelocity;
+//    Vector3 velocity;
+
+//    Controller2D controller;
+        
+//	void Start () {
+//        controller = GetComponent<Controller2D> ();
+
+//        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+//        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+//        print("Gravity: " + gravity + " Jump Velocity: " + jumpVelocity);
+//	}
+
+//    void Update()
+//    {
+//        if(controller.collisions.above || controller.collisions.below)
+//        {
+//            velocity.y = 0;
+//        }
+
+//        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+//        if (Input.GetButtonDown("Jump") && controller.collisions.below)
+//        {
+//            velocity.y = jumpVelocity;
+//        }
+
+//        float targetVelocityX = input.x * moveSpeed;
+//        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+//        velocity.y += gravity * Time.deltaTime;
+//        controller.Move(velocity * Time.deltaTime);
+//    }
+//}
--- Assets/Scripts/PlayerController-old.cs
+++ Assets/Scripts/PlayerController-old.cs
@@ -2,98 +2,104 @@
 //using System.Collections.Generic;
 //using UnityEngine;
 
-//public class PlayerController : MonoBehaviour {
+//public class PlayerController-old : MonoBehaviour
+//{
 
-//	//how fast the player can move
-//	public float topSpeed = 10f;
-//	//tell the sprite which direction it is facing
-//	bool facingRight = true;
-//	//get reference to animator
-//	Animator anim;
-//	//not grounded
-//	bool grounded = false;
-//	//transform at players foot to see if he is touching the ground
-//	public Transform groundCheck;
-//	//how big the circle is going to be when we check distance to the ground
-//	public float groundRadius = 0.2f;
-//	// force of jump
-//	public float jumpForce = 700f;
-//	//what layer is the ground
-//	public LayerMask whatIsGround;
-//	//variable to check double jump
-//	public bool doubleJump;
+//    //how fast the player can move
+//    public float topSpeed = 10f;
+//    //tell the sprite which direction it is facing
+//    bool facingRight = true;
+//    //get reference to animator
+//    Animator anim;
+//    //not grounded
+//    bool grounded = false;
+//    //transform at players foot to see if he is touching the ground
+//    public Transform groundCheck;
+//    //how big the circle is going to be when we check distance to the ground
+//    public float groundRadius = 0.2f;
+//    // force of jump
+//    public float jumpForce = 700f;
+//    //what layer is the ground
+//    public LayerMask whatIsGround;
+//    //variable to check double jump
+//    public bool doubleJump;
 //    public GameObject forceField;
 
 
 
-//    void Start() {
-//		anim = GetComponent<Animator> ();
-//	}
-
-
-//	void FixedUpdate() {
-
-//		//true or false did the ground transform hit the whatIsGround with the groundRadius
-//		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
-
-//		//get direction
-//		float move = Input.GetAxis("Horizontal");
-
-//		//tell the animator we are grounded
-//		anim.SetBool ("Ground", grounded);
-
-//		//get how fast we are moving up or down from the rigidbody
-//		anim.SetFloat ("vSpeed", GetComponent<Rigidbody2D> ().velocity.y);
-
-
-//		//add velocity to the rigidbody in the move direction * our speed
-//		GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
-//		anim.SetFloat("Speed", Mathf.Abs (move));
-
-//		//if we're facing the negative direction and not facing right, flip
-//		if (move > 0 && !facingRight)
-//			Flip ();
-//		else if (move < 0 && facingRight)
-//			Flip ();
-//	}
-
-//	void Update()
+//    void Start()
 //    {
-//		if (Input.GetButtonDown ("Jump"))
-//        {
-//			if(grounded)
-//            {
-//				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, jumpForce), ForceMode2D.Impulse);
-//				doubleJump = true;
-//			} else {
-//				if(doubleJump)
-//                {
-//					doubleJump = false;
-//					GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x, 0);
-//					GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, jumpForce), ForceMode2D.Impulse);
+//        anim = GetComponent<Animator>();
+//    }
 
-//				}
-//			}
-//		}
+
+//    void FixedUpdate()
+//    {
+
+//        //true or false did the ground transform hit the whatIsGround with the groundRadius
+//        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+//        //get direction
+//        float move = Input.GetAxis("Horizontal");
+
+//        //tell the animator we are grounded
+//        anim.SetBool("Ground", grounded);
+
+//        //get how fast we are moving up or down from the rigidbody
+//        anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+
+
+//        //add velocity to the rigidbody in the move direction * our speed
+//        GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+//        anim.SetFloat("Speed", Mathf.Abs(move));
+
+//        //if we're facing the negative direction and not facing right, flip
+//        if (move > 0 && !facingRight)
+//            Flip();
+//        else if (move < 0 && facingRight)
+//            Flip();
+//    }
+
+//    void Update()
+//    {
+//        if (Input.GetButtonDown("Jump"))
+//        {
+//            if (grounded)
+//            {
+//                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+//                doubleJump = true;
+//            }
+//            else
+//            {
+//                if (doubleJump)
+//                {
+//                    doubleJump = false;
+//                    GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
+//                    GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+
+//                }
+//            }
+//        }
 
 //        Physics2D.IgnoreCollision(forceField.GetComponent<Collider2D>(), GetComponent<BoxCollider2D>());
 //        Physics2D.IgnoreCollision(forceField.GetComponent<Collider2D>(), GetComponent<CircleCollider2D>());
 
 //    }
 
-//	void Flip() {
+//    void Flip()
+//    {
 
-//		//saying we are facing the opposite direction
-//		facingRight = !facingRight;
+//        //saying we are facing the opposite direction
+//        facingRight = !facingRight;
 
-//		//get the local scale
-//		Vector3 theScale = transform.localScale;
+//        //get the local scale
+//        Vector3 theScale = transform.localScale;
 
-//		//flip on the x axis
-//		theScale.x *= -1;
+//        //flip on the x axis
+//        theScale.x *= -1;
 
-//		//apply that to the local scale
-//		transform.localScale = theScale;
-//	}
+//        //apply that to the local scale
+//        transform.localScale = theScale;
+//    }
 //}
